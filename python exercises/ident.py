3616f39ddf1567b7de7aff5cda8cdfbc18029e47
sasdelliju <faustosasdelli@gmail.com>
2022-11-24 11:47:12 +0000

    python exercises

---
+++ python exercises/ident.py
@@ -0,0 +1,14 @@
+RUN_INDENTED = True
+
+message = "running unindentend"
+
+if RUN_INDENTED:
+    message = "running indented"
+
+print(message)
+
+def my_function():
+    greet = "Hello"
+    return greet
+
+print(my_function())
